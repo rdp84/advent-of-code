995ef11a5f70957fcbef4b15b796c07d98aa2dc9
Bobby Pearson <robert.pearson@sky.uk>
2025-11-25 18:09:44 +0000

    part 1

--- 2024/17/day_17.c
+++ 2024/17/day_17.c
@@ -11,33 +11,80 @@ int main(void)
 
 void part_1(void)
 {
-  char *prg;
-  int a, b, c;
+  char *prg, opcode, oprnd;
+  int ip, reg[REGISTERS];
+  output *output;
 
-  a = b = c = 0;
-  prg = alloc_program(&a, &b, &c);
-  printf("a: %d\nb: %d\nc: %d\nprg: %s\n", a, b, c, prg);
+  prg = alloc_program(reg);
+  ip = 0;
+  output = NULL;
+  printf("Input:\nRegisters - a:%d, b:%d, c:%d\nProgram - %s\n", reg[0], reg[1], reg[2], prg);
+
+  while ((opcode = *(prg + ip)) != '\0') {
+    oprnd = *(prg + ip + 1);
+
+    switch (opcode) {
+    case '0':
+      adv(reg, oprnd);
+      ip += 2;
+      break;
+    case '1':
+      bxl(reg, oprnd);
+      ip += 2;
+      break;
+    case '2':
+      bst(reg, oprnd);
+      ip += 2;
+      break;
+    case '3':
+      jnz(&ip, reg, oprnd);
+      break;
+    case '4':
+      bxc(reg);
+      ip += 2;
+      break;
+    case '5':
+      output = out(reg, oprnd, output);
+      ip += 2;
+      break;
+    case '6':
+      bdv(reg, oprnd);
+      ip += 2;
+      break;
+    case '7':
+      cdv(reg, oprnd);
+      ip += 2;
+      break;
+    default:
+      printf("ERROR: did not recognise opcode");
+      free(prg);
+      return;
+    }
+  }
+
+  printf("\nOutput:\nRegisters - a:%d, b:%d, c:%d\n", reg[0], reg[1], reg[2]);
+  print_output(output);
+  free_output(output);
   free(prg);
 }
 
-char *alloc_program(int *a, int *b, int *c)
+char *alloc_program(int *reg)
 {
   FILE *ifp;
   size_t linecap;
   ssize_t linelen;
-  char *retValue, *line, *toFree, *pos, *num, chr, reg;
+  char *retValue, *line, *toFree, *pos, *num, chr;
   int len, i;
 
   line = NULL;
   linecap = 0;
-  ifp = fopen("example_input.txt", "r");
+  ifp = fopen("input.txt", "r");
 
   while ((linelen = getline(&line, &linecap, ifp)) > 1) {
     toFree = line;
     while ((chr = *(line++)) != A && chr != B && chr != C)
       ;
 
-    reg = chr;
     line++;
     pos = line;
     while (*line++ != '\n')
@@ -47,12 +94,12 @@ char *alloc_program(int *a, int *b, int *c)
     num = malloc(len * sizeof(char));
     strlcpy(num, pos, len);
 
-    if (reg == A)
-      *a = atoi(num);
-    else if (reg == B)
-      *b = atoi(num);
+    if (chr == A)
+      *reg = atoi(num);
+    else if (chr == B)
+      *(reg + 1) = atoi(num);
     else
-      *c = atoi(num);
+      *(reg + 2) = atoi(num);
 
     free(num);
     free(toFree);
@@ -80,4 +127,108 @@ char *alloc_program(int *a, int *b, int *c)
   free(toFree);
   fclose(ifp);
   return retValue;
+}
+
+void free_output(output *o)
+{
+  output *toFree;
+
+  while (o != NULL) {
+    toFree = o;
+    o = o->next;
+    free(toFree);
+  }
+}
+
+void print_output(output *o)
+{
+  while (o != NULL) {
+    printf("%d%s", o->n, o->next != NULL ? "," : "\n");
+    o = o->next;
+  }
+}
+
+void adv(int *reg, char oprnd)
+{
+  int den;
+
+  if (is_literal(oprnd))
+    den = pow(2, char_to_int(oprnd));
+  else
+    den = pow(2, *(reg + get_reg(oprnd)));
+
+  *reg /=  den;
+}
+
+void bxl(int *reg, char oprnd)
+{
+  *(reg + 1) = *(reg + 1) ^ char_to_int(oprnd);
+}
+
+void bst(int *reg, char oprnd)
+{
+  *(reg + 1) = is_literal(oprnd) ?
+    (char_to_int(oprnd)) % 8 :
+    *(reg + (get_reg(oprnd))) % 8;
+}
+
+void jnz(int *ip, int *reg, char oprnd)
+{
+  *ip = *reg != 0 ? char_to_int(oprnd) : *ip + 2;
+}
+
+void bxc(int *reg)
+{
+  *(reg + 1) = *(reg + 1) ^ *(reg + 2);
+}
+
+output *out(int *reg, char oprnd, output *o)
+{
+  int n;
+  output *retValue;
+
+  n = (is_literal(oprnd)) ?
+    (char_to_int(oprnd)) % 8 :
+    *(reg + (get_reg(oprnd))) % 8;
+
+  if (o == NULL) {
+    retValue = malloc(sizeof(output));
+    retValue->n = n;
+    retValue->next = NULL;
+  }
+  else {
+    retValue = o;
+    while (o->next != NULL)
+      o = o->next;
+    o->next = malloc(sizeof(output));
+    o = o->next;
+    o->n = n;
+    o->next = NULL;
+  }
+
+  return retValue;
+}
+
+void bdv(int *reg, char oprnd)
+{
+  int den;
+
+  if (is_literal(oprnd))
+    den = pow(2, char_to_int(oprnd));
+  else
+    den = pow(2, *(reg + get_reg(oprnd)));
+
+  *(reg + 1) = *reg / den;
+}
+
+void cdv(int *reg, char oprnd)
+{
+  int den;
+
+  if (is_literal(oprnd))
+    den = pow(2, char_to_int(oprnd));
+  else
+    den = pow(2, *(reg + get_reg(oprnd)));
+
+  *(reg + 2) = *reg / den;
 }
--- 2024/17/day_17.h
+++ 2024/17/day_17.h
@@ -5,11 +5,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <math.h>
+
+#define REGISTERS 3
 
 #define A 'A'
 #define B 'B'
 #define C 'C'
 
-char *alloc_program(int *, int *, int *);
+#define is_literal(oprnd)  oprnd >= '0' && oprnd <= '3'
+#define char_to_int(oprnd) oprnd - '0'
+#define get_reg(oprnd)     ((char_to_int(oprnd)) % 4)
+
+struct output {
+  int n;
+  struct output *next;
+};
+typedef struct output output;
+
+
+char *alloc_program(int *);
+void free_output(output *);
+void print_output(output *);
+
+void   adv(int *, char);
+void   bxl(int *, char);
+void   bst(int *, char);
+void   jnz(int *, int *, char);
+void   bxc(int *);
+output *out(int *, char, output *);
+void   bdv(int *, char);
+void   cdv(int *, char);
 
 #endif
